9dfa1493c94f467c0e9e0376a6f5545ac26bed81
Clicli <19654092+Furikuda@users.noreply.github.com>
2025-03-14 15:47:51 +0100

    init

---
+++ tgclient/client.py
@@ -0,0 +1,174 @@
+import ctypes
+from ctypes.util import find_library
+import json
+import os
+
+
+from queue import Queue
+
+
+class TelegramError(Exception):
+    pass
+
+class TelegramClient:
+    def __init__(self, api_id, api_hash, tdlib_database_directory=None):
+        self.log_file = open("/tmp/tdlog.json", "ab+")
+
+        self.tdlib_database_directory = tdlib_database_directory
+        if not tdlib_database_directory:
+            self.tdlib_database_directory = 'tdlib'
+
+        self.api_id = api_id
+        self.api_hash = api_hash
+
+        self._init_tdlib()
+
+        self._init_client()
+
+    def _init_client(self):
+        self.td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1, '@extra': 1.01234})
+        # create client
+        self.client_id = self._td_create_client_id()
+        # another test for TDLib execute method
+        print(str(self.td_execute({'@type': 'getTextEntities', 'text': '@telegram /test_command https://telegram.org telegram.me', '@extra': ['5', 7.0, 'a']})).encode('utf-8'))
+
+        # start the client by sending a request to it
+        self.td_send({'@type': 'getOption', 'name': 'version', '@extra': 1.01234})
+
+
+    # simple wrappers for client usage
+    def td_send(self, query):
+        query = json.dumps(query).encode('utf-8')
+        self._td_send(self.client_id, query)
+
+    def td_receive(self):
+        result = self._td_receive(1.0)
+        if result:
+            result = json.loads(result.decode('utf-8'))
+        return result
+
+    def td_execute(self, query):
+        query = json.dumps(query).encode('utf-8')
+        result = self._td_execute(query)
+        if result:
+            result = json.loads(result.decode('utf-8'))
+        return result
+
+    def _init_tdlib(self):
+        # load shared library
+        tdjson_path = find_library('tdjson')
+        if tdjson_path is None:
+            if os.name == 'nt':
+                tdjson_path = os.path.join(os.path.dirname(__file__), 'tdjson.dll')
+            else:
+                raise TelegramError("Can't find 'tdjson' library")
+        tdjson = ctypes.CDLL(tdjson_path)
+
+        # load TDLib functions from shared library
+        self._td_create_client_id = tdjson.td_create_client_id
+        self._td_create_client_id.restype = ctypes.c_int
+        self._td_create_client_id.argtypes = []
+
+        self._td_receive = tdjson.td_receive
+        self._td_receive.restype = ctypes.c_char_p
+        self._td_receive.argtypes = [ctypes.c_double]
+
+        self._td_send = tdjson.td_send
+        self._td_send.restype = None
+        self._td_send.argtypes = [ctypes.c_int, ctypes.c_char_p]
+
+        self._td_execute = tdjson.td_execute
+        self._td_execute.restype = ctypes.c_char_p
+        self._td_execute.argtypes = [ctypes.c_char_p]
+
+        self.log_message_callback_type = ctypes.CFUNCTYPE(None, ctypes.c_int, ctypes.c_char_p)
+
+        _td_set_log_message_callback = tdjson.td_set_log_message_callback
+        _td_set_log_message_callback.restype = None
+        _td_set_log_message_callback.argtypes = [ctypes.c_int, self.log_message_callback_type]
+
+#        _td_set_log_message_callback(2, on_log_message_callback)
+
+    def log(self, event):
+        j = json.dumps(event).encode('utf-8')
+        self.log_file.write(j+b"\n")
+        self.log_file.flush()
+
+    def handle_event(self, event):
+        if event['@type'] == 'updateUser':
+            uid = event['user']['id']
+            self.td_send({'@type': 'getGroupsInCommon', 'user_id': uid, 'offset': 0, 'limit': 20, '@extra': ['pouet', uid, 1.01234]})
+        if event['@type'] == 'updateAuthorizationState':
+            auth_state = event['authorization_state']
+
+            # if client is closed, we need to destroy it and create new client
+            if auth_state['@type'] == 'authorizationStateClosed':
+                return
+
+            # set TDLib parameters
+            # you MUST obtain your own api_id and api_hash at https://my.telegram.org
+            # and use them in the setTdlibParameters call
+            if auth_state['@type'] == 'authorizationStateWaitTdlibParameters':
+                self.td_send({'@type': 'setTdlibParameters',
+                         'database_directory': self.tdlib_database_directory,
+                         'use_message_database': True,
+                         'use_secret_chats': True,
+                         'api_id': self.api_id,
+                         'api_hash': self.api_hash,
+                         'system_language_code': 'en',
+                         'device_model': 'Desktop',
+                         'application_version': '1.0'})
+
+            # enter phone number to log in
+            if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
+                phone_number = input('Please enter your phone number: ')
+                self.td_send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
+
+            # enter email address to log in
+            if auth_state['@type'] == 'authorizationStateWaitEmailAddress':
+                email_address = input('Please enter your email address: ')
+                self.td_send({'@type': 'setAuthenticationEmailAddress', 'email_address': email_address})
+
+            # wait for email authorization code
+            if auth_state['@type'] == 'authorizationStateWaitEmailCode':
+                code = input('Please enter the email authentication code you received: ')
+                self.td_send({'@type': 'checkAuthenticationEmailCode',
+                         'code': {'@type': 'emailAddressAuthenticationCode', 'code' : code}})
+
+            # wait for authorization code
+            if auth_state['@type'] == 'authorizationStateWaitCode':
+                code = input('Please enter the authentication code you received: ')
+                self.td_send({'@type': 'checkAuthenticationCode', 'code': code})
+
+            # wait for first and last name for new users
+            if auth_state['@type'] == 'authorizationStateWaitRegistration':
+                first_name = input('Please enter your first name: ')
+                last_name = input('Please enter your last name: ')
+                self.td_send({'@type': 'registerUser', 'first_name': first_name, 'last_name': last_name})
+
+            # wait for password if present
+            if auth_state['@type'] == 'authorizationStateWaitPassword':
+                password = input('Please enter your password: ')
+                self.td_send({'@type': 'checkAuthenticationPassword', 'password': password})
+
+#        # initialize TDLib log with desired parameters
+#        @self.log_message_callback_type
+#        def on_log_message_callback(_, verbosity_level, message):
+#            if verbosity_level == 0:
+#                sys.exit(f'TDLib fatal error: {message}')
+
+    # main events cycle
+    def main_loop(self, handle_event=None):
+        if not handle_event:
+            handle_event = self.handle_event
+        while True:
+            event = self.td_receive()
+            if event:
+                handle_event(event)
+#                self.log(event)
+
+if __name__ == "__main__":
+    with open("config.json", 'r') as f:
+        conf = json.load(f.read())
+        t = TelegramClient(conf['api_id'], conf['api_hash'])
+        t.main_loop()
---
+++ tgclient/main.py
@@ -0,0 +1,105 @@
+from multiprocessing import Process, Queue
+
+import json
+
+from textual import on, work
+from textual.app import App, ComposeResult
+from textual.containers import VerticalScroll
+from textual.message import Message
+from textual.screen import ModalScreen, Screen
+from textual.widgets import Button, Footer, Header, Label, Input, RichLog
+
+import client
+
+class PopUp(Screen[str]):
+
+    def __init__(self, question: str, placeholder='') -> None:
+        self.question = question
+        self.placeholder = placeholder
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        yield Label(self.question)
+        yield Input(placeholder=self.placeholder)
+        yield Button("Yes", id="yes", variant="success")
+
+    @on(Button.Pressed, "#yes")
+    def handle_yes(self, event):
+        self.dismiss("coin")
+
+#    @on(Input.Submitted)
+#    def on_input(self) -> None:
+#        input_text = self.query_one(Input)
+#        text = input_text.value
+#        self.dismiss(text)
+
+
+class Main(App):
+    CSS_PATH = "main.tcss"
+    BINDINGS = [
+        ("q", "quit", "Quit"),
+    ]
+
+    def __init__(self, api_id, api_hash):
+        super().__init__()
+
+        self.tgclient = None
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.processes = []
+
+    def compose(self) -> ComposeResult:
+        """Compose our UI."""
+
+        yield Header()
+        with VerticalScroll(id="telegram"):
+            yield RichLog(id="telegram-client", auto_scroll=True)
+        yield Footer()
+
+    async def _ask_question_str(self, s):
+        return await self.push_screen_wait(PopUp(s))
+
+#    @on(LogMessage)
+#    def handle_event(self, event: LogMessage):
+#        phone_number =  self._ask_question_str('Please enter your phone number')
+#        text_log = self.query_one(RichLog)
+#        j = json.dumps(event.event).encode('utf-8')
+#        text_log.write(j)
+
+    async def _handle_tg_event(self, event):
+
+
+        phone_number = await self._ask_question_str('Please enter your phone number')
+#        text_log = self.query_one(RichLog)
+#        j = json.dumps(event).encode('utf-8')
+#        text_log.write(j)
+#        if event['@type'] == 'updateAuthorizationState':
+#            auth_state = event['authorization_state']
+#            if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
+#                text_log.write('We need to ask for phone number')
+#                phone_number =  self._ask_question_str('Please enter your phone number')
+#                self.tgclient.td_send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
+#        else:
+#            self.tgclient.handle_event(event)
+
+
+    def _background_tgworker_start(self):
+        self.tgclient.main_loop(handle_event=self._handle_tg_event)
+
+    def on_mount(self):
+        self.tgclient = client.TelegramClient(self.api_id, self.api_hash)
+
+        tg_events_queue = Queue()
+
+        tgworker = Process(target=self.tgclient.main_loop, args=(tg_events_queue))
+        tgworker.start()
+        self.processes.append(tgworker)
+
+
+
+if __name__ == "__main__":
+    with open("config.json", 'r') as f:
+        conf = json.load(f.read())
+
+        m = Main(conf['api_id'], conf['api_hash'])
+        m.run()
